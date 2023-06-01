bf64e26c2d5f6f63d5e4be89b39e33db3481228a
Peter Runge <prunge@causal.net.au>
2023-06-01 23:02:54 +1000

    A completely local test with Spring/Webflux/Netty.

---
+++ integration-tests/spring/src/test/java/au/net/causal/hymie/spring/TestSpring.java
@@ -0,0 +1,72 @@
+package au.net.causal.hymie.spring;
+
+import io.netty.channel.ChannelOption;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+
+import java.net.URI;
+
+import static org.assertj.core.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT,
+                classes = TestSpring.TestConfiguration.class,
+                properties = {"server.port=8080"}
+)
+@AutoConfigureMockMvc
+class TestSpring
+{
+    @LocalServerPort
+    private int serverPort;
+
+    @Test
+    void test()
+    {
+        HttpClient httpClient = HttpClient.create()
+                                          .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 5000);
+
+        WebClient client = WebClient.builder()
+                                    .clientConnector(new ReactorClientHttpConnector(httpClient))
+                                    .build();
+
+        var r = client.get().uri(URI.create("http://localhost:" + serverPort + "/hello"));
+
+        String response =
+                r.retrieve()
+                 .bodyToMono(String.class)
+                 .block();
+
+        assertThat(response).isEqualTo("Good morning, what will be for eating?");
+    }
+
+    @SpringBootApplication
+    public static class TestConfiguration
+    {
+        @Bean
+        public MyRestController myRestController()
+        {
+            return new MyRestController();
+        }
+    }
+
+    @RestController
+    public static class MyRestController
+    {
+        @GetMapping("/hello")
+        public String hello()
+        {
+            return "Good morning, what will be for eating?";
+        }
+    }
+}
