e573c128504ead1f1bcc7883a0b65ca3bd6a55d7
Peter Runge <prunge@causal.net.au>
2023-06-05 18:54:26 +1000

    Check results from integration tests.

--- integration-tests/spring/src/test/java/au/net/causal/hymie/spring/CheckAgentResultsIT.java
+++ integration-tests/spring/src/test/java/au/net/causal/hymie/spring/CheckAgentResultsIT.java
@@ -1,12 +1,26 @@
 package au.net.causal.hymie.spring;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import java.nio.file.Path;
+
+import static org.assertj.core.api.Assertions.*;
 
 class CheckAgentResultsIT
 {
+    private static Path hymieLog;
+
+    @BeforeAll
+    static void setUp()
+    {
+        hymieLog = Path.of(System.getProperty("hymie.exerciser.log", "target/exerciser-hymie.log"));
+    }
+
     @Test
     void test()
     {
-        System.out.println("Check output from agent after the server is shut down");
+        System.out.println("Check agent output file " + hymieLog);
+        assertThat(hymieLog).exists().isNotEmptyFile();
     }
 }
